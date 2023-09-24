8ef7edc32a7748b1cbca73011e8ed4c56f4f9145
hhminh87 <144453316+hhminh87@users.noreply.github.com>
2023-09-24 12:15:43 +0700

    Add files via upload

---
+++ Lession01/Bai tajp Ve hinh tam giac.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	//Bài 1: Su dung kien thuc da hoc ve hinh sau
+//	int i,j, N, AB, AC;
+//	//Nhap canh AB va AC cua tam giac vuong ABC
+//	printf("\nNhap do dai canh AB= ");
+//	scanf("\n%d", &AB);
+//	printf("\nNhap do dai canh AC= ");
+//	scanf("\n%d", &AC);
+//	// Ve tam gia vuong can ABC
+//	printf("B");
+//	printf("\n");
+//	for (i=0; i<AB; i++)
+//		{
+//			for (j=0; j<i; j++)
+//				{
+//					printf("*");
+//				}
+//			printf("\n");
+//		}
+//	printf("A       C");
+//		// Bai 2 Ve hinh
+//		/* ===================================
+//
+//| STT | Ho Ten            | Tuoi     |  Gioi Tinh       |
+//
+//|01     | Nguyen A        | 20       | Nam               |
+//
+//|02     | Nguyen B        | 22       | Nam               |
+//
+//|03     | Nguyen C        | 26       | Nam               | */
+//
+//printf("\n|STT  |Ho ten     |Tuoi  |Gioi tinh   |");
+//printf("\n|01   |Nguyen A   |20    |Nam         |");
+//printf("\n|02   |Nguyen B   |22    |Nam	        |");
+//printf("\n|03   |Nguyen C   |26    |nam         |");
+// Bai 3 Tinh gia tri bieu thuc
+//d = 10 * a * a + 5 * b * c + c * c;
+
+
+//int a=1, b=1, c=1,d;
+//d=10*pow(a,2)+5*b*c+pow(c,2);
+//printf("\n%d", d);
+//	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
