43969694030809a9916d99db5eee190fe09087d1
hhminh87 <144453316+hhminh87@users.noreply.github.com>
2023-09-25 09:20:18 +0700

    Add files via upload

---
+++ Lession01/Bien va kieu du lieuj voi scanf va printf.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	/*
+	Vi?t chuong trình nh?p vào m?t s? a b?t k? và in ra giá tr? a2, a3, a4
+Vi?t chuong trình d?c t? bàn phím 3 s? nguyên bi?u di?n ngày, tháng, nam và xu?t ra màn hình du?i d?ng “dd/mm/yyyy”.
+Vi?t chuong trình d?c và 2 s? th?c vào in ra k?t qu? c?a phép (+), phép tr? (-), phép nhân (*), phép chia (/). */
+//int a,b,s, a2,a3,a4;
+//printf("\nNhap so a= ");
+//scanf("%d", &a);
+//printf("\nNhap so b= ");
+//scanf("%d", &b);
+//a2=pow(a,2);
+//printf("\na2= %d ", a2);
+//printf("\na3= %d ", a3=pow(a,3));
+//printf("\na4= %d ", a4=pow(a,4));
+// ngay than nam
+//int dd,mm,yyyy;
+//printf("\nnhap dd: ");
+//scanf("%d", &dd);
+//printf("\nNhap mm: ");
+//scanf("%d", &mm);
+//printf("\nNhap yyyy: ");
+//scanf("%d", &yyyy);
+//printf("\n %d/%d/%d", dd,mm,yyyy);
+float a,b;
+printf("\nnhap so thuc a= ");
+scanf("%f", a)
+
+
+
+
+
+	return 0;
+}
